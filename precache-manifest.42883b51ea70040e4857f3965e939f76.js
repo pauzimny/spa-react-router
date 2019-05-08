c4af63339f92cdc198b54e572a119e8e14d653d5
Paulina Zimny <paulina.zimny@gmail.com>
2019-05-08 13:34:38 +0200

    Updates

---
+++ precache-manifest.42883b51ea70040e4857f3965e939f76.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3b57b28e3dc21796390",
+    "url": "/spa-react-router/static/css/main.0769f8fb.chunk.css"
+  },
+  {
+    "revision": "b3b57b28e3dc21796390",
+    "url": "/spa-react-router/static/js/main.ee3002e3.chunk.js"
+  },
+  {
+    "revision": "3f2b9c9f3c89f0566a74",
+    "url": "/spa-react-router/static/js/runtime~main.c18a6f22.js"
+  },
+  {
+    "revision": "90e68d51b99ed36e34c4",
+    "url": "/spa-react-router/static/js/2.496dae61.chunk.js"
+  },
+  {
+    "revision": "8c44c4592f2890385691f8ae8b2404be",
+    "url": "/spa-react-router/static/media/cosmetic-2357981_1920.8c44c459.jpg"
+  },
+  {
+    "revision": "4271a249b689f905ea65b3608c50ce08",
+    "url": "/spa-react-router/static/media/essential-oil-3816410_1920.4271a249.jpg"
+  },
+  {
+    "revision": "d982fd0a6ca09494b98d56d5e6322f28",
+    "url": "/spa-react-router/static/media/herbs-3119132_1920.d982fd0a.jpg"
+  },
+  {
+    "revision": "78975b59c8cde06a83d149040568ed65",
+    "url": "/spa-react-router/index.html"
+  }
+];
